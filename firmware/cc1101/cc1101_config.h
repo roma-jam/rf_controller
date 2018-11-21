93272addbdaad0c033bf9aafa29169ec05ed84d9
roma jam <jam_roma@yahoo.com>
2018-11-22 01:05:44 +0300

    + Switch off some debug information on CC1101 level.

--- firmware/cc1101/cc1101_config.h
+++ firmware/cc1101/cc1101_config.h
@@ -14,9 +14,9 @@
 
 // DEBUG
 #define CC1101_DEBUG                            1
-#define CC1101_DEBUG_REQUESTS                   1
+#define CC1101_DEBUG_REQUESTS                   0
 #define CC1101_DEBUG_ERRORS                     1
-#define CC1101_DEBUG_FLOW                       1
+#define CC1101_DEBUG_FLOW                       0
 #define CC1101_DEBUG_INFO                       1
 
 
